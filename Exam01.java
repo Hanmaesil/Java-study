334cf882c2a3d9ee2ffb0c243e9e784d6fe42d42
Hanmaesil <93114691+Hanmaesil@users.noreply.github.com>
2021-11-17 12:45:45 +0900

    Add files via upload

--- Exam01.java
+++ Exam01.java
@@ -1,20 +1,29 @@
 
 public class Exam01 {
-//main + ctrl + space로 메인 만들수 있음.
+
 	public static void main(String[] args) {
-	
-		//문자열 변수
-		String name = "한국인";
-		System.out.println(name);
 		
-		//상수 : 변하지 않는 값을 가진 것.
-		final int age = 29;
+		//, /**/ 주석
 		
+		//출력문 : 어떤 값을 console로 보는 방법.
+		//System.out.println("Hello World!!!"); //문자열은 "" 를 사용. 실행 단축키 ctrl + F11
 		
+		//변수 : 데이터를 저장할 수 있는 일시적인 공간
+		//변수 작성 : 자료형 변수명 대입연산자 값;
+		//예시 : 정수형 변수 a 에다가 3을 대입하시오
+		int a = 100;
+		System.out.println(a);
+		System.out.println(a); //단축키 syso + ctrl + space
+		System.out.println(a); //줄복사 단축키 ctrl + art + 아래화살표 
+		System.out.println(a); 
+		System.out.println(a); 
+		System.out.println(a); 
+		System.out.println(a); 
+		System.out.println(a); 
+		System.out.println(a); 
+		System.out.println(a); 
 		
-		
-		
+
 	}
+
 }
-
-
